e6dcc727a2c25d4f64dc19ef1cb8ed84bf97b95b
Ted Staggs <72319119+tedstaggs01@users.noreply.github.com>
2023-07-27 14:10:15 +0200

    d

--- functions/mb.js
+++ functions/mb.js
@@ -6,38 +6,40 @@ function calculateResult() {
                 var bem = 647; 
                 var bemcg = 2.112; 
                 var bemmo = bem * bemcg; 
-                var crew = document.getElementById("crew").value;
+                console.log(bem);
+                var crew = parseFloat(document.getElementById("crew").value);
                 var crewcg = 2.05; 
                 var crewmo = crew * crewcg; 
-                var pax = document.getElementById("pax").value;
+                var pax = parseFloat(document.getElementById("pax").value);
                 var paxcg = 2.993; 
                 var paxmo = pax * paxcg; 
-                var bag = document.getElementById("bag").value;
+                var bag = parseFloat(document.getElementById("bag").value);
                 var bagcg = 3.627; 
                 var bagmo = bag * bagcg; 
                 var zfm = bem + crew + pax + bag;
                 var zfmmo = bemmo + crewmo + paxmo + bagmo;
                 var zfmcg = zfmmo / zfm; 
                 document.getElementById("zfm").innerHTML = "Ramp mass =" + zfm +" | Ramp CG = " + zfmcg;
-                var totfuel = document.getElementById("totfuel").value;
+                var totfuel = parseFloat(document.getElementById("totfuel").value);
                 var totfuelcg = 2.413; 
                 var totfuelmo = totfuel * totfuelcg; 
                 var ramp = zfm + totfuel;
                 var rampmo = zfmmo + totfuelmo;
                 var rampcg = rampmo / ramp;  
-                var taxfuel = document.getElementById("taxfuel").value;
+                var taxfuel = parseFloat(document.getElementById("taxfuel").value);
                 var taxfuelcg = totfuelcg; 
                 var taxfuelmo = taxfuel * taxfuelcg; 
                 var tom = ramp - taxfuel; 
                 var tommo = rampmo - taxfuelmo;
                 var tomcg = tommo / tom;  
-                var trpfuel = document.getElementById("trpfuel").value;
+                var trpfuel = parseFloat(document.getElementById("trpfuel").value);
                 var trpfuelcg = totfuelcg; 
                 var trpfuelmo = trpfuel * trpfuelcg; 
                 var ldg = tom - trpfuel; 
                 var ldgmo = tommo - trpfuelmo;
                 var ldgcg = ldgmo / ldg;  
-            } 
+            }
+            lxaib();  
             break;
 
         case "lxaif":
